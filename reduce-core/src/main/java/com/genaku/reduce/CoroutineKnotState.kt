37924367b33774a3d797f0221858a421342b15ff
Gena Kuchergin <gena_kuchergin@mail.ru>
2021-06-30 23:58:05 +0300

    remove reentrant lock

--- reduce-core/src/main/java/com/genaku/reduce/CoroutineKnotState.kt
+++ reduce-core/src/main/java/com/genaku/reduce/CoroutineKnotState.kt
@@ -7,14 +7,11 @@ import kotlin.concurrent.write
 
 class CoroutineKnotState<S : State>(initialState: S) : KnotState<S> {
 
-//    private val lock = ReentrantReadWriteLock()
     private val _state = MutableStateFlow(initialState)
 
     override val state: StateFlow<S> = _state
 
     suspend fun changeState(newState: suspend (S) -> S) {
-//        lock.write {
-            _state.emit(newState(_state.value))
-//        }
+        _state.emit(newState(_state.value))
     }
 }
