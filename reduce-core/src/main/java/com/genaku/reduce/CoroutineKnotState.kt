0024ede47d89034f782fde5c3255da5c06938b25
Gena Kuchergin <gena_kuchergin@mail.ru>
2021-07-28 08:31:54 +0300

    thread safe emit of new state

--- reduce-core/src/main/java/com/genaku/reduce/CoroutineKnotState.kt
+++ reduce-core/src/main/java/com/genaku/reduce/CoroutineKnotState.kt
@@ -2,8 +2,6 @@ package com.genaku.reduce
 
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
-import java.util.concurrent.locks.ReentrantReadWriteLock
-import kotlin.concurrent.write
 
 class CoroutineKnotState<S : State>(initialState: S) : KnotState<S> {
 
@@ -12,6 +10,16 @@ class CoroutineKnotState<S : State>(initialState: S) : KnotState<S> {
     override val state: StateFlow<S> = _state
 
     suspend fun changeState(newState: suspend (S) -> S) {
-        _state.emit(newState(_state.value))
+        _state.update { newState(_state.value) }
+    }
+
+    private inline fun <T> MutableStateFlow<T>.update(function: (T) -> T) {
+        while (true) {
+            val prevValue = value
+            val nextValue = function(prevValue)
+            if (compareAndSet(prevValue, nextValue)) {
+                return
+            }
+        }
     }
 }
