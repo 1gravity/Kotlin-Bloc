bc544937d4fbb4c7b7cbbe034d4553f3e839fd86
Emanuel Moecklin <emanuel.moecklin@gmail.com>
2022-04-06 15:00:04 -0400

    experiment with a 'pass-through' redux store (State == Action)

--- bloc-samples/src/commonMain/kotlin/com/onegravity/bloc/sample/counter/ReduxCounter.kt
+++ bloc-samples/src/commonMain/kotlin/com/onegravity/bloc/sample/counter/ReduxCounter.kt
@@ -22,8 +22,19 @@ object ReduxCounter {
 }
 
 // TODO think about the initial value, who/what provides it? should be have a mechanism to populate
-//      the BlocState upon start?
+//      the BlocState upon start? especially ReduxBlocState has TWO initial values (one for the
+//      ReduxStore and one in the BlocState)
 
 // TODO think about navigation as well, will side effects be sufficient?
 
 // TODO iOS sample app
+
+// TODO implement a ReduxStore with State = Action along the line of:
+//  ```
+//  inline fun <reified State: Any> BlocContext.reduxBlocState(initialState: State): BlocState<State, State> {
+//        val reducer: Reducer<State> = { state: State, action: Any ->
+//            if (action is State) action else state
+//     }
+//        return createThreadSafeStore(reducer, initialState).toBlocState(this, initialState)
+//  }
+//  ```
