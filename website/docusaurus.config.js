24a4c06d2850d8249b828b4953cd275c435aa8a2
Emanuel Moecklin <1gravityllc@gmail.com>
2022-06-21 07:39:42 -0400

    add Algolia credentials

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -127,12 +127,12 @@ const config = {
       },
       algolia: {
         // The application ID provided by Algolia
-        appId: 'YOUR_APP_ID',
+        appId: 'R1VNVBGTR1',
   
         // Public API key: it is safe to commit it
-        apiKey: 'YOUR_SEARCH_API_KEY',
+        apiKey: 'c06f93c57a0719879e5a0b8c6b766f62',
   
-        indexName: 'YOUR_INDEX_NAME',
+        indexName: 'kotlin-bloc',
   
         // Optional: see doc section below
         contextualSearch: true,
