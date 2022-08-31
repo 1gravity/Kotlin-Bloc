956dcef770956b3542de870d012b1e701ab147cd
Emanuel Moecklin <1gravityllc@gmail.com>
2022-08-31 11:48:32 -0400

    compose 1.3.0 and Kotlin 1.7.10 will do the job

--- bloc-compose/build.gradle.kts
+++ bloc-compose/build.gradle.kts
@@ -63,7 +63,7 @@ android {
     }
 
     composeOptions {
-        kotlinCompilerExtensionVersion = "1.2.0"
+        kotlinCompilerExtensionVersion = "1.3.0"
         useLiveLiterals = true
     }
 }
--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -9,6 +9,6 @@ repositories {
 }
 
 dependencies {
-    api("org.jetbrains.kotlin:kotlin-gradle-plugin:1.7.0")
+    api("org.jetbrains.kotlin:kotlin-gradle-plugin:1.7.10")
     api("com.android.tools.build:gradle:7.2.2")
 }
