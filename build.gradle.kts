3d3e26621629b75264139759cd92ca96f5a7ee19
Emanuel Moecklin <1gravityllc@gmail.com>
2022-09-27 10:24:37 -0400

    minor cleanup

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,17 +4,16 @@ buildscript {
     repositories {
         gradlePluginPortal()
         mavenCentral()
+        google()
         maven("https://oss.sonatype.org/content/repositories/snapshots")
         maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
         maven("https://jitpack.io")
-        google()
     }
     dependencies {
         classpath(Android.tools.build.gradlePlugin)
         classpath(AndroidX.navigation.safeArgsGradlePlugin)
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:_")
         classpath("org.jetbrains.compose:compose-gradle-plugin:_")
-        classpath(Android.tools.build.gradlePlugin)
         classpath(Square.sqlDelight.gradlePlugin)
     }
 }
@@ -32,10 +31,10 @@ tasks.dokkaHtmlMultiModule.configure {
 allprojects {
     repositories {
         mavenCentral()
+        google()
         maven("https://oss.sonatype.org/content/repositories/snapshots")
         maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
         maven("https://jitpack.io")
-        google()
     }
 
     afterEvaluate {
