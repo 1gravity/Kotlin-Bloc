68a7b57eedb3209671221f7be0ad541f6d830835
Emanuel Moecklin <emanuel.moecklin@vivint.com>
2023-06-27 23:13:12 +0000

    Revert "delete obsolete class ReducerContainer"
    This reverts commit d1a8324003fe621e1cc3db3377705696442b18f3.

---
+++ bloc-core/src/commonMain/kotlin/com/onegravity/bloc/internal/ReducerContainer.kt
@@ -0,0 +1,12 @@
+package com.onegravity.bloc.internal
+
+import com.onegravity.bloc.utils.Effect
+import com.onegravity.bloc.utils.ReducerNoAction
+
+/**
+ * Wrapper class for reducers that are submitted Redux style (send(Action)) or MVVM+ style (reduce { })
+ */
+internal data class ReducerContainer<State : Any, Action : Any, SideEffect : Any, Proposal : Any>(
+    val action: Action? = null,
+    val reducer: ReducerNoAction<State, Effect<Proposal, SideEffect>>? = null
+)
